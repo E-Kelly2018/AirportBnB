53b22dbc9ee1ed5b23cfcda935276ff2e54445b7
Eoin Kelly <k.eoin2000@gmail.com>
2022-01-31 16:43:15 +0000

    Added testing to codebase

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -3,23 +3,22 @@ package forms
 import (
 	"fmt"
 	"github.com/asaskevich/govalidator"
-	"net/http"
 	"net/url"
 	"strings"
 )
 
-//Form creates a cutom form struct embeds a url.Values object
+// Form creates a custom form struct and embeds a url.Values object
 type Form struct {
 	url.Values
 	Errors errors
 }
 
-//Valid returns true if there is no errors otherwise return false
+// Valid returns true if there are no errors, otherwise false
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
 }
 
-//New initializes a form struct
+// New initializes a form struct
 func New(data url.Values) *Form {
 	return &Form{
 		data,
@@ -27,35 +26,38 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Required checks for required fields
 func (f *Form) Required(fields ...string) {
 	for _, field := range fields {
 		value := f.Get(field)
 		if strings.TrimSpace(value) == "" {
-			f.Errors.Add(field, "This field can not be blank")
+			f.Errors.Add(field, "This field cannot be blank")
 		}
 	}
 }
 
-//Has checks if form field is in post and not empty
-func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
+// Has checks if form field is in post and not empty
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
 	if x == "" {
 		return false
 	}
 	return true
 }
 
-//MinLength Checks for fields min length
-func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	fieldName := r.Form.Get(field)
-	if len(fieldName) < length {
+// MinLength check for minimum length
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
+	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
 		return false
 	}
 	return true
 }
+
+// IsEmail checks for a valid email address
 func (f *Form) IsEmail(field string) {
 	if !govalidator.IsEmail(f.Get(field)) {
-		f.Errors.Add(field, "Invalid Email Address")
+		f.Errors.Add(field, "Invalid email address")
 	}
 }
